34de7d94c5532f96daeb55b24a62555a866dfc26
VGuseinov <vladislav.guslan@gmail.com>
2022-07-21 21:17:42 +0300

    lessonten

---
+++ IJProject]/src/com/company/ATM.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ATM {
+    private Bank bank;
+    private Scanner sc;
+
+    public ATM(Bank bank) {
+        this.bank = bank;
+        this.sc = new Scanner(System.in);
+    }
+
+    public void start() {
+        // TODO: доделать потом
+    }
+}
